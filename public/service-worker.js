acef38baa0aaa6d2c6c32e9a6abb199cfccd0b24
bperez05 <perez.brandon55@yahoo.com>
2022-01-24 16:54:23 -0600

    adeed activation evnt listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,6 +10,7 @@ const FILES_TO_CACHE = [
   ];
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE = "data-cache-v1";
+
   // install
   self.addEventListener("install", function (evt) {
     evt.waitUntil(
@@ -18,4 +19,22 @@ const FILES_TO_CACHE = [
       })
     );
     self.skipWaiting();
+  });
+  
+  // activate
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
   });
