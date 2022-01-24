0496b8362601278728b49d42a4341f479ea3e1c5
bperez05 <perez.brandon55@yahoo.com>
2022-01-24 15:35:46 -0600

    function to creat object store

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -3,3 +3,14 @@ let db;
 let tx;
 let store;
 let index;
+
+request.onupgradeneeded = function (e) {
+    let db = request.result;
+    store = db.createObjectStore("moneySpent", {
+      autoIncrement: true,
+    });
+  };
+  
+  request.onerror = function (e) {
+    console.log("There was an error:" + e.target.errorCode);
+  };
